57aa39cd71d7c4c44972f5b550ab9e187b148440
Berack96 <giacomobertolazzi7@gmail.com>
2025-09-04 21:55:18 +0200

    Added examples of agno usage and .env for keys; removed unused/redundant dependencies

---
+++ src/example.py
@@ -0,0 +1,20 @@
+from agno.agent import Agent
+from agno.models.google import Gemini
+from agno.tools.reasoning import ReasoningTools
+from dotenv import load_dotenv
+
+try:
+    load_dotenv()
+
+    reasoning_agent = Agent(
+        model=Gemini(),
+        tools=[
+            ReasoningTools(),
+        ],
+        instructions="Use tables to display data.",
+        markdown=True,
+    )
+    result = reasoning_agent.run("Scrivi una poesia su un gatto. Sii breve.")
+    print(result.content)
+except Exception as e:
+    pass
