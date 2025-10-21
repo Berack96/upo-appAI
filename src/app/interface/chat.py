d24459b9a77afc89d0837cee262034ef6262caa9
Berack96 <giacomobertolazzi7@gmail.com>
2025-10-21 16:57:28 +0200

    Refactor configs dei modelli + fix chat interface default selection

--- src/app/interface/chat.py
+++ src/app/interface/chat.py
@@ -84,14 +84,16 @@ class ChatManager:
                     label="Modello da usare"
                 )
                 provider.change(fn=self.inputs.choose_team_leader, inputs=provider, outputs=None)
+                provider.value = self.inputs.team_leader_model.label
 
-                style = gr.Dropdown(
+                strategy = gr.Dropdown(
                     choices=self.inputs.list_strategies_names(),
                     value=self.inputs.strategy.label,
                     type="index",
                     label="Stile di investimento"
                 )
-                style.change(fn=self.inputs.choose_strategy, inputs=style, outputs=None)
+                strategy.change(fn=self.inputs.choose_strategy, inputs=strategy, outputs=None)
+                strategy.value = self.inputs.strategy.label
 
             chat = gr.ChatInterface(
                 fn=self.gradio_respond
